945a19f332ad6a46f8038122cfcd23704dea3467
Jordan Jordan Katie <jordantauscher@tuta.io>
2022-09-22 21:14:29 +0100

    correct ghostscript bin path in bash script

--- lib/PDF-Compressor/compress.sh
+++ lib/PDF-Compressor/compress.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 INPUT_FILE=$1
 OUTPUT_FILE=$2
-exec ./bin/gs -sDEVICE=pdfwrite -q -dCompatibilityLevel=$4 -r$3 -dNOPAUSE -dBATCH -dSAFER -dOverprint=/$5 -sDEVICE=pdfwrite -dPDFSETTINGS=/$6 -dEmbedAllFonts=$7 -dSubsetFonts=$8 -dAutoRotatePages=/$9 -dColorImageDownsampleType=/${10} -dColorImageResolution=${11} -dGrayImageDownsampleType=/${10} -dGrayImageResolution=${11} -dMonoImageDownsampleType=/${10} -dMonoImageResolution=${11} -sOutputFile="$OUTPUT_FILE" "$INPUT_FILE"
+exec ./lib/bin/gs -sDEVICE=pdfwrite -q -dCompatibilityLevel=$4 -r$3 -dNOPAUSE -dBATCH -dSAFER -dOverprint=/$5 -sDEVICE=pdfwrite -dPDFSETTINGS=/$6 -dEmbedAllFonts=$7 -dSubsetFonts=$8 -dAutoRotatePages=/$9 -dColorImageDownsampleType=/${10} -dColorImageResolution=${11} -dGrayImageDownsampleType=/${10} -dGrayImageResolution=${11} -dMonoImageDownsampleType=/${10} -dMonoImageResolution=${11} -sOutputFile="$OUTPUT_FILE" "$INPUT_FILE"
