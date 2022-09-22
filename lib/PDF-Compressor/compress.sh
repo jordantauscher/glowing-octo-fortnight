95d9a03a300973d91887a7c7375fe9fc7e594df4
jordantauscher <111900813+jordantauscher@users.noreply.github.com>
2022-09-22 21:03:53 +0100

    Update compress.sh

--- lib/PDF-Compressor/compress.sh
+++ lib/PDF-Compressor/compress.sh
@@ -1,3 +1,3 @@
 INPUT_FILE=$1
 OUTPUT_FILE=$2
-./bin/gs -sDEVICE=pdfwrite -q -dCompatibilityLevel=$4 -r$3 -dNOPAUSE -dBATCH -dSAFER -dOverprint=/$5 -sDEVICE=pdfwrite -dPDFSETTINGS=/$6 -dEmbedAllFonts=$7 -dSubsetFonts=$8 -dAutoRotatePages=/$9 -dColorImageDownsampleType=/${10} -dColorImageResolution=${11} -dGrayImageDownsampleType=/${10} -dGrayImageResolution=${11} -dMonoImageDownsampleType=/${10} -dMonoImageResolution=${11} -sOutputFile="$OUTPUT_FILE" "$INPUT_FILE"
+exec ./bin/gs -sDEVICE=pdfwrite -q -dCompatibilityLevel=$4 -r$3 -dNOPAUSE -dBATCH -dSAFER -dOverprint=/$5 -sDEVICE=pdfwrite -dPDFSETTINGS=/$6 -dEmbedAllFonts=$7 -dSubsetFonts=$8 -dAutoRotatePages=/$9 -dColorImageDownsampleType=/${10} -dColorImageResolution=${11} -dGrayImageDownsampleType=/${10} -dGrayImageResolution=${11} -dMonoImageDownsampleType=/${10} -dMonoImageResolution=${11} -sOutputFile="$OUTPUT_FILE" "$INPUT_FILE"
